9c26d2ce1f95a39d62c14c8d676412ffeff848b2
MauryIV <mauryhughesiv@gmail.com>
2024-05-07 14:26:10 -0600

    correction in webpack and incuded cache in src-se.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
 
 module.exports = () => {
   return {
