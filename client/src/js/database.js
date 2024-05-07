fcd7c05e50a0904122976c90194264530956a367
MauryIV <mauryhughesiv@gmail.com>
2024-05-07 15:04:43 -0600

    completed the necessary db functions in the database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,25 +1,48 @@
-// TODO: Install the following package:
 import { openDB } from 'idb';
 
-// TODO: Complete the initDb() function below:
 const initdb = async () => {
-
+  openDB('contacts', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('contacts')) {
+        console.log('contacts database already exists');
+        return;
+      }
+      db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true });
+      console.log('contacts database created');
+    },
+  });
 };
 
-
-// TODO: Complete the postDb() function below:
 export const postDb = async (name, home, cell, email)  => {
- 
+  console.log('Post contact');
+  const contactsDb = await openDB('contacts', 1);
+  const tx = contactsDb.transaction('contacts', 'readwrite');
+  const store = tx.objectStore('contacts');
+  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+  const result = await request;
+  console.log('Contact saved!', result);
 };
 
-// TODO: Complete the getDb() function below:
 export const getDb = async () => {
-  
+  console.log('Get all contacts');
+  const contactsDb = await openDB('contacts', 1);
+  const tx = contactsDb.transaction('contacts', 'readonly');
+  const store = tx.objectStore('contacts');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result
 };
 
-// TODO: Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+  console.log('Delete contact', id);
+  const contactsDb = await openDB('contacts', 1);
+  const tx = contactsDb.transaction('contacts', 'readwrite');
+  const store = tx.objectStore('contacts');
+  const request = store.delete(id);
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value
 };
 
 initdb();
